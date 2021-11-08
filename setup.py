a408947fde9632b2e02bc841900e83fb3c240dbc
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2021-11-08 12:40:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 import os, platform
+from setuptools import setup
 if platform.system().lower() == "windows":
-  
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
   os.system("pip install -r requirements.txt")
 else:
   os.system("pip3 install -r requirements.txt")
+ 
+setup(name="pihome", version="1.0.0", packages=["pihome.server", "pihome"], license="MIT", install_requires=requirements, classifiers=['Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: All',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: MUSIC',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed'])
