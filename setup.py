54382f356c08d2ac1533be90ca07cdb8a46eb8bd
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2021-11-08 13:11:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pychromecast
 requirements = []
 
 requirements = req.splitlines()
-setup(name="pihome", version="1.0.3", packages=["pihome"], license="MIT", install_requires=requirements, classifiers=['Development Status :: 5 - Production/Stable',
+setup(name="pihome", version="1.0.4beta", packages=["pihome"], license="MIT", install_requires=requirements, classifiers=['Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Natural Language :: English',
