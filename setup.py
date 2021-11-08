74e36970e0099ee128c8b88b6dbccfaca36ad33e
StarNumber <francesco.lumaca2009@gmail.com>
2021-11-08 12:58:47 +0100

    bruh!

--- setup.py
+++ setup.py
@@ -4,14 +4,11 @@ req = """
 flask 
 pychromecast 
 """
-if platform.system().lower() == "windows":
-  os.system("pip install -r requirements.txt")
-else:
-  os.system("pip3 install -r requirements.txt")
+
 requirements = []
 
 requirements = req.splitlines()
-setup(name="pihome", version="1.0.0", packages=["."], license="MIT", install_requires=requirements, classifiers=['Development Status :: 5 - Production/Stable',
+setup(name="pihome", version="1.0.2", packages=["server"], license="MIT", install_requires=requirements, classifiers=['Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Natural Language :: English',
